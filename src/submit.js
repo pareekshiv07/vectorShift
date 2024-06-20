f24eeacc4ab4ca41879bcf549c65d9be60b9580c
pareekshiv <pareekshiv07@gmail.com>
2024-06-20 15:40:10 +0530

    task 4 completed of assignment

--- src/submit.js
+++ src/submit.js
@@ -1,10 +1,32 @@
-// submit.js
+import axios from 'axios';
+import { useStore } from './store';
+import { useState } from 'react';
 
 export const SubmitButton = () => {
+  const { nodes, edges } = useStore((state) => ({
+    nodes: state.nodes,
+    edges: state.edges,
+  }));
 
-    return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
-        </div>
-    );
-}
+  const [response, setResponse] = useState(null);
+
+  const handleSubmit = async () => {
+    const pipeline = { nodes, edges };
+
+    try {
+      const res = await axios.post('http://localhost:8000/pipelines/parse', pipeline);
+      const data = res.data;
+      setResponse(data);
+      alert(`Number of Nodes: ${data.num_nodes}, Number of Edges: ${data.num_edges}, Is DAG: ${data.is_dag}`);
+    } catch (error) {
+      console.error('Failed to fetch:', error);
+      alert('Failed to fetch data from server.');
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <button type="button" onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
